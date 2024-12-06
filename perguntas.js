0c76f600a2a85af1a0e612c263cd19d16977ca7e
Tatiane T. Pinto <143770937+proftati2@users.noreply.github.com>
2024-12-06 10:56:17 -0300

    Publicação do flashcards

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Pensamento Computacional?',
+    'É uma habilidade para resolver problemas e desafios de forma eficiente, assim como um computador faria'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'Qual a importância da Biotecnologia?',
+    'A biotecnologia possibilita o desenvolvimento de vacinas e testes diagnósticos, produtos de extrema importância no atual contexto em que estamos vivendo'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor de 2 + 5 x 3 + 4?',
+    '21'
+)
+
+criaCartao(
+    'Química',
+    'Qual o tipo de misturas?',
+    'Homogêneas e Heterogêneas'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'O que é empreendedorismo?',
+    'é a capacidade que uma pessoa tem de identificar problemas e oportunidades, desenvolver soluções e investir recursos na criação de algo positivo para a sociedade. '
+)
